967707f46aa5173ea1583e672bb88b74a06ac285
邵汉中 <635260101@qq.com>
2021-06-09 18:41:47 +0800

    update

--- src/main/java/com/shz/queue/ConsumerAssignDemo.java
+++ src/main/java/com/shz/queue/ConsumerAssignDemo.java
@@ -1,38 +1,40 @@
 package com.shz.queue;
 
-import com.github.luben.zstd.ZstdOutputStream;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.TopicPartition;
 import org.apache.kafka.common.serialization.StringDeserializer;
-import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.time.Duration;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Properties;
 import java.util.regex.Pattern;
 
-public class ConsumerDemo {
+public class ConsumerAssignDemo {
+
     public static void main(String[] args) {
         Properties props = new Properties();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,"node01:9092,node02:9092,node03:9092");
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "node01:9092,node02:9092,node03:9092");
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        props.put(ConsumerConfig.GROUP_ID_CONFIG,"g1");
         KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(props);
 
-        // 订阅topics
-        consumer.subscribe(Pattern.compile("^topic.*"));
+        // 订阅topics，手动指定topic的分区
+        TopicPartition partition = new TopicPartition("topic03", 0);
+        List<TopicPartition> partitions = Arrays.asList(partition);
+        consumer.assign(partitions);
+        // 从第一条消息读起
+        //consumer.seekToBeginning(partitions);
+        consumer.seek(partition,13);
 
         // 遍历消息队列
-        while (true){
+        while (true) {
             ConsumerRecords<String, String> records = consumer.poll(Duration.ofSeconds(1));
-            if(!records.isEmpty()){
-                records.forEach(r-> System.out.println(r));
+            if (!records.isEmpty()) {
+                records.forEach(r -> System.out.println("topic=" + r.topic() + ",partition=" + r.partition() + ",key=" + r.key() + ",value=" + r.value()));
             }
         }
-
     }
 }
---
+++ src/main/java/com/shz/queue/ConsumerGroupDemo.java
@@ -0,0 +1,35 @@
+package com.shz.queue;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import java.time.Duration;
+import java.util.Properties;
+import java.util.regex.Pattern;
+
+public class ConsumerGroupDemo {
+    /*
+     * 启动多个消费者组实例，由Kafka协调消费者与分区的对应关系，同组下的所有消费者均分收到的消息，并且组内有序
+     * */
+    public static void main(String[] args) {
+        Properties props = new Properties();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "node01:9092,node02:9092,node03:9092");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "g1");
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(props);
+
+        // 订阅topics，由Kafka协调消费者与分区的对应关系。当组内的某个消费者宕机后，kafka会自动重新分配消费者与分区对应关系
+        consumer.subscribe(Pattern.compile("^topic.*"));
+
+        // 遍历消息队列
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofSeconds(1));
+            if (!records.isEmpty()) {
+                records.forEach(r -> System.out.println("topic=" + r.topic() + ",partition=" + r.partition() + ",key=" + r.key() + ",value=" + r.value()));
+            }
+        }
+    }
+}
